b9caf2c790e16b69e3674263ef12f2564799a738
abdeon23 <76174268+abdeon23@users.noreply.github.com>
2026-01-11 19:15:34 +0100

    Add files via upload

--- server_datex2.py
+++ server_datex2.py
@@ -143,5 +143,6 @@ def ranking():
 # INICIO
 # -------------------
 if __name__ == "__main__":
-    db.create_all()
+    with app.app_context():
+        db.create_all()
     app.run(host="0.0.0.0", port=5000, debug=True)
